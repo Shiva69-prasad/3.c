2320cdc9f3e91dd3cd5880d633656d9a960c7326
Shiva69-prasad <shiva31694@gmail.com>
2025-09-06 22:07:52 +0530

    3.c

---
+++ 3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    printf("%d\n",(int) sizeof(int));
+    printf("%hi\n",(int) sizeof(unsigned int));
+    printf("%d\n",(int) sizeof(short));
+    printf("%d\n",(int) sizeof(unsigned short));
+    printf("%li\n",(long) sizeof(long));
+    printf("%lu\n",(long) sizeof(unsigned long));
+    printf("%f\n",(float) sizeof(float));
+
+ printf("%d\n",(int) sizeof(char));
+
+
+}
